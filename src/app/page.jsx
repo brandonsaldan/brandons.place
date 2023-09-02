dfad778b6f3a3a8fe00d582bd8b8fb3306b67d99
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2023-09-01 22:36:35 -0400

    test: og-image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import clsx from 'clsx'
+import Head from 'next/head'
 
 import { Button } from '@/components/Button'
 import { Card } from '@/components/Card'
@@ -316,6 +317,35 @@ export default async function Home() {
 
   return (
     <>
+    <Head>
+    <title>GeneCodex</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta charSet="utf-8" />
+      <meta name="keywords" content="genecodex, snpedia" />
+      <meta name="author" content="Brandon Saldan" />
+      <meta name="robots" content="index, follow" />
+      <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta httpEquiv="content-language" content="en" />
+      <meta
+        name="description"
+        content="GeneCodex is a free, open-source tool that analyzes your DNA data and provides you with a report of your genetic variants."
+      />
+      <link rel="icon" href="/images/logo.png" />
+      {/* Open Graph / Facebook */}
+      <meta property="og:image" content="https://raw.githubusercontent.com/brandonsaldan/codex/main/public/images/og-image.png" />
+      <meta property="og:image:width" content="1760"/>
+      <meta property="og:image:height" content="880"/>
+      <meta property="og:title" content="GeneCodex" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://codex-brandonsaldan.vercel.app/" />
+      <meta property="og:description" content="GeneCodex is a free, open-source tool that analyzes your DNA data and provides you with a report of your genetic variants." />
+      {/* Open Graph / Twitter */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:domain" content="https://codex-brandonsaldan.vercel.app/" />
+      <meta name="twitter:title" content="GeneCodex" />
+      <meta name="twitter:description" content="GeneCodex is a free, open-source tool that analyzes your DNA data and provides you with a report of your genetic variants." />
+      <meta name="twitter:image" content="https://raw.githubusercontent.com/brandonsaldan/codex/main/public/images/og-image.png" />
+    </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
