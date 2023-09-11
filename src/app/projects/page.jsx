5ce906b488e5c7db25b29bacd94011bd62c621ea
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2023-09-11 17:52:39 -0400

    fix: errors

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -156,7 +156,7 @@ function LinkIcon(props) {
 
 function XIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-x">
       <path d="M18 6 6 18"/>
       <path d="m6 6 12 12"/>
     </svg>
