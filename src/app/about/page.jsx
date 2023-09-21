6a1241cebf3a453bbfd8d6de4a60f5a866ce850e
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2023-09-21 18:06:33 -0400

    fix: only rotate image on desktop

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -57,7 +57,7 @@ export default function About() {
               src={portraitImage}
               alt=""
               sizes="(min-width: 1024px) 32rem, 20rem"
-              className="aspect-square rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+              className="aspect-square sm:rotate-3 rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
             />
           </div>
         </div>
