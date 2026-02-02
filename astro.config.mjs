a051180810430ad01728ed9c6383465eba89c0ae
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2026-02-02 00:12:20 -0500

    fix: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://brandonsaldan.com',
     integrations: [
         mdx(),
         sitemap(),
